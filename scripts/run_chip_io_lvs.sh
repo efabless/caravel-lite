c837c5ea979bc6bf96df33b1db17860e8a92168b
R. Timothy Edwards <tim@opencircuitdesign.com>
2022-04-19 22:05:27 -0400

    Numerous bug fixes, ending in clean full LVS for both caravel and caravan. (#76)
    * (1) Modified the .magicrc file to set a default for PDK if not set in the
    environment.  (2) Fixed the user ID programming layout to not leave holes
    behind when the script moves the vias around (similar to the handling of
    the GPIO defaults block).  (3) Added substrate isolation to gpio_control_block
    and fixed the path references to the standard cells.  (4) Fixed the four
    missing routes on the Caravan top level.  (5) Reinstated the large rendered
    labels for the pads on both caravel and caravan.  (6) Corrected the top
    level gate-level netlist for caravan to add the missing pins to the
    management core wrapper.  (7) Did the same for the caravan top level RTL.
    (8) Created scripts to run full LVS including extracting the management
    core wrapper and reading all gate-level verilog submodules.  (9) Moved all
    of the LVS scripts to the scripts directory.
    
    * Apply automatic changes to Manifest and README.rst
    
    * Made the changes from pull request #73 as they did not get merged
    successfully, and if merged now they will generate conflicts with
    this pull request in scripts/set_user_id.py.  So it's easier to
    just manually add them to this pull request.
    
    * Apply automatic changes to Manifest and README.rst
    
    Co-authored-by: RTimothyEdwards <RTimothyEdwards@users.noreply.github.com>

---
+++ scripts/run_chip_io_lvs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+echo ${PDK_ROOT:=/usr/share/pdk} > /dev/null
+
+if [ ! -f caravel.spice ]; then
+magic -dnull -noconsole -rcfile $PDK_ROOT/$PDK/libs.tech/magic/$PDK.magicrc << EOF
+drc off
+crashbackups stop
+load caravel
+select top cell
+expand
+extract do local
+extract all
+ext2spice lvs
+ext2spice
+EOF
+rm -f *.ext
+fi
+
+export NETGEN_COLUMNS=60
+netgen -batch lvs "caravel.spice chip_io" "../verilog/gl/chip_io.v chip_io" $PDK_ROOT/$PDK/libs.tech/netgen/$PDK_VARIENT_setup.tcl comp.out
